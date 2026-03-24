8d3d0a2e38780383ff57491c5d2681051516ea39
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-03-24 10:38:18 +0100

    Make native WrapMetalTexture no-op when SK_METAL is disabled

--- skiko/src/nativeJsMain/cpp/gpu/graphite/BackendTexture.cc
+++ skiko/src/nativeJsMain/cpp/gpu/graphite/BackendTexture.cc
@@ -3,16 +3,21 @@
 
 #ifdef SK_METAL
 #include "gpu/graphite/mtl/MtlGraphiteTypes_cpp.h"
+#endif
 
 SKIKO_EXPORT KNativePointer org_jetbrains_skia_gpu_graphite_BackendTexture__1nWrapMetalTexture(KNativePointer texturePtr, KInt width, KInt height){
+#ifdef SK_METAL
     skgpu::graphite::BackendTexture backendTexture = skgpu::graphite::BackendTextures::MakeMetal(
             SkISize::Make(width, height),
             reinterpret_cast<CFTypeRef>(texturePtr)
     );
 
     return reinterpret_cast<KNativePointer>(new skgpu::graphite::BackendTexture(backendTexture));
-}
+#else
+    return static_cast<KNativePointer>(nullptr);
 #endif
+}
+
 
 static void deleteBackendTexture(skgpu::graphite::BackendTexture* backendTexture) {
     delete backendTexture;
