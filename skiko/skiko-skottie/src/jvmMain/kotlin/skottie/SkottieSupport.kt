8993852bb7e7a4e4df6d833988484a2425a0796f
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-04-26 01:36:54 +0200

    Fix skottie logger

--- skiko/skiko-skottie/src/jvmMain/kotlin/skottie/SkottieSupport.kt
+++ skiko/skiko-skottie/src/jvmMain/kotlin/skottie/SkottieSupport.kt
@@ -1,4 +1,5 @@
 package org.jetbrains.skia.skottie
+import org.jetbrains.skia.ExternalSymbolName
 import org.jetbrains.skiko.hostId
 import org.jetbrains.skiko.LibraryLoader
 import org.jetbrains.skiko.LockFile
@@ -14,5 +15,8 @@ actual object SkottieLibrary {
     actual fun load() {
         Library.load()
         loadNativeLibrary("skiko-skottie-$hostId")
+        _nAfterLoad()
     }
 }
+
+private external fun _nAfterLoad()
