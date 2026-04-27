00b21518f2135be9e42a8388628b43e070f42c6d
Hubert Błaszczyk <hubertblaszczyk03@gmail.com>
2026-04-27 16:21:34 +0200

    Another xml filter

--- skiko/buildSrc/src/main/kotlin/tasks/GenerateSymbolsListTask.kt
+++ skiko/buildSrc/src/main/kotlin/tasks/GenerateSymbolsListTask.kt
@@ -82,6 +82,7 @@ abstract class GenerateSymbolsListTask : DefaultTask() {
         val keepSet = extImportedList
             .filter { it in coreExportsSet }
             .filter { !it.startsWith("XML_") }
+            .filter { !it.startsWith("Xml") }
             .filter { !it.startsWith("FT_") }
             .filter { !it.startsWith("Cr_") }
             .toSet()
