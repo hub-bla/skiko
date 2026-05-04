6741a484c1a82dba6f176ae51e88985adb1e1b29
Hubert Blaszczyk <hubert.blaszczyk@jetbrains.com>
2026-05-04 15:53:27 +0200

    Add resource path for web targets

---
+++ skiko/skiko-skottie/src/wasmJsTest/kotlin/org/jetbrains/skiko/tests/TestUtils.wasm.kt
@@ -0,0 +1,25 @@
+package org.jetbrains.skiko.tests
+
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.await
+import kotlinx.coroutines.promise
+import org.jetbrains.skiko.wasm.awaitSkiko
+
+actual annotation class SkipJsTarget
+
+actual typealias SkipWasmTarget = kotlin.test.Ignore
+
+@JsFun("() => awaitSkiko")
+private external fun awaitSkiko(): JsAny
+
+@JsFun("() => ''")
+private external fun jsRef(): JsAny
+
+/**
+ * Runs the [block] in a coroutine.
+ */
+actual fun runTest(block: suspend () -> Unit): TestReturnType = MainScope().promise {
+    awaitSkiko.await<Any>()
+    block()
+    jsRef()
+}
